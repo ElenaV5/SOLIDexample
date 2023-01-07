72c3e68cc92dd1742e156581abf55ee7ece01071
ElenaVN <elenavnev@gmail.com>
2023-01-07 23:37:04 +0500

    OCP right example

---
+++ app/src/main/java/ru/synergy/solidexample/ocp/OrderProcessorWithPreAndPostProcessing.java
@@ -0,0 +1,21 @@
+package ru.synergy.solidexample.ocp;
+
+import ru.synergy.solidexample.models.Order;
+import ru.synergy.solidexample.srp.right.OrderProcessor;
+
+public class OrderProcessorWithPreAndPostProcessing  extends OrderProcessor {
+    @Override
+    public void process(Order order){
+        beforeProcessing();
+        super.process(order);
+        afterProcessing();
+    }
+
+    private void beforeProcessing() {
+        //Осуществим некоторые действия перед обработкой заказа
+    }
+
+    private void afterProcessing() {
+        //Осуществим некоторые действия после обработки заказа
+    }
+}
